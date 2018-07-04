7f9f51814eec4910cc7533ac4c111a10fb94a958
Matt Williamson <mwilliamson@ucdavis.edu>
2018-07-04 09:47:56 -0600

    Create 24_PPMOccurrenceUnburned.R

---
+++ code/24_PPMOccurrenceUnburned.R
@@ -0,0 +1,21 @@
+#estimate the posterior predictive mass for regression parameters in the occurrence model for unburned data
+library(rstan)
+library(bayesplot)
+library(ggplot2)
+library(gridExtra)
+infolder <- "/Users/matthewwilliamson/Google Drive/GB_Final/model_fits/"
+outfolder <- "/Users/matthewwilliamson/Google Drive/GB_Final/summary_plots/"
+load(paste0(infolder, "occ_fit_ub_hillshade.RData")) #load model fit data
+db.stan <- read.csv("/Users/matthewwilliamson/Google Drive/GB_Final/processed_data/occurence_model_ub_hillshade_noscale.csv")
+
+#rename slope estimates (beta's)
+names(occ_fit_ub)[391:395] <- c("Elevation",  "Hillshade", "Winter precip.", "Winter precip./Total precip.", "Grazed")
+
+#Extract posterior samples for each slope parameter
+posterior <- as.data.frame(occ_fit_ub, pars=c("beta_p", "beta_o"))
+#Percentage of posterior samples greater or less than zero
+perc_pos <- apply(posterior, 2, function(x) (length(which(x > 0))/length(x) * 100))
+perc_neg <- apply(posterior, 2, function(x) (length(which(x < 0))/length(x) * 100))
+
+mass_sum <- cbind(perc_pos, perc_neg)
+write.csv(mass_sum, file=paste0(outfolder,"Occ_UB_hillshade_MassSum.csv"))
