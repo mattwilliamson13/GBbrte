7d825379dbbc21b6659048a45047e70a5f104fa6
Matt Williamson <mwilliamson@ucdavis.edu>
2018-07-03 20:11:55 -0600

    Create 16_PPCPrevalenceUnburned.R

---
+++ code/16_PPCPrevalenceUnburned.R
@@ -0,0 +1,58 @@
+library(loo)
+library(bayesplot)
+infolder <- "/Users/matthewwilliamson/Google Drive/GB_Final/model_fits/"
+outfolder <- "/Users/matthewwilliamson/Google Drive/GB_Final/ppc_reprate/"
+
+load(paste0(infolder, "reprate_fit_ub_hillshade.RData")) #load model fit data
+db.stan <- read.csv(file="/Users/matthewwilliamson/Google Drive/GB_Final/processed_data/reprate_model_unburned_hillshade.csv", stringsAsFactors = FALSE)
+
+###density plot overlay
+y_ppcs <- as.matrix(reprate_fit_ub, pars = "y_tilde")
+ppc_dens_overlay(
+  y = db.stan$brte,
+  yrep = y_ppcs[sample(nrow(y_ppcs), 100),]
+)
+
+###Test statistic graphics
+ppc_stat(
+  y = db.stan$brte,
+  yrep = y_ppcs,
+  stat="mean",
+  binwidth = 0.05
+)
+
+ppc_stat(
+  y = db.stan$brte,
+  yrep = y_ppcs,
+  stat="median",
+  binwidth = 0.05
+)
+
+ppc_stat(
+  y = db.stan$brte,
+  yrep = y_ppcs,
+  stat="sd",
+  binwidth = 0.05
+)
+
+ppc_stat(
+  y = db.stan$brte,
+  yrep = y_ppcs,
+  stat="max",
+  binwidth = 0.05
+)
+
+##Numerical fit estimates
+rr_sim <- rstan::extract(reprate_fit_ub)
+
+od2 <- rr_sim$obs_dev2
+rd2 <- rr_sim$sim_dev2
+fit <- apply(od2, 2, sum)
+fit.new <- apply(rd2, 2, sum)
+gof <- mean(ifelse((fit.new - fit) > 0, 1, 0)) # 0.31211
+
+##LOO estimate provides an estimate of the number of points that are extreme in light of the model
+log_lik_fit <-  extract_log_lik(reprate_fit_ub, parameter_name = "log_lik")
+
+loo_fit <- loo(log_lik_fit)
+
