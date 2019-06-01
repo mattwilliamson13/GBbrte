0b017eaecd54cbf9bf174fd65362f69973cced76
mattwilliamson13 <mwilliamson@ucdavis.edu>
2019-06-01 16:37:05 -0700

    fix pathnames

--- code/02_AddPrecipAspect.R
+++ code/02_AddPrecipAspect.R
@@ -1,10 +1,10 @@
 ## This code is to generate mean precip values and hillshade values for use as point-level covariates
 library(tidyverse)
 
-infolder <- "/Users/matthewwilliamson/Google Drive/GB_Final/processed_data/" #data is already processed so both inputs and outputs in processed folder
-outfolder <- "/Users/matthewwilliamson/Google Drive/GB_Final/processed_data/"
+infolder <- here::here("processed_data/") #data is already processed so both inputs and outputs in processed folder
+outfolder <- here::here("processed_data/")
 #load the combined dataset
-db.or <-read.csv(file =paste0(infolder,"alldatamerged.csv"))
+db.or <-read.csv(file =paste0(infolder,"/alldatamerged.csv"))
 
 db.pcp <- db.or %>% group_by(pt) %>% summarise(medwtpcp = median(wtpr, na.rm = TRUE),
                                                 medsppcp = median(sppr, na.rm = TRUE))
@@ -14,8 +14,8 @@ db.proppcp <- db.or %>% group_by(pt, yr) %>% summarise(propwtr = wtpr/(wtpr+sppr
 precip <- unique(db.or[,c("range","area", "pt")]) %>%
             left_join(db.pcp, by="pt") %>%
             left_join(db.proppcp, by="pt")
-aspect <- read.csv(file="/Users/matthewwilliamson/Google Drive/GB_Final/original_data/Export_vegpts_hillshade_30m.csv") #southwesternnes from Matthias Leu
+aspect <- read.csv(file=here::here("original_data/Export_vegpts_hillshade_30m.csv"), stringsAsFactors = FALSE) #southwesternnes from Matthias Leu
 
 pt_covs <- aspect[,c(1,2,3,6)] %>% left_join(precip, by=c("range", "area","pt"))
 colnames(pt_covs)[4] <- "hillshade"
-write.csv(pt_covs, paste0(outfolder,"med_pcp_hillshade.csv"), row.names=FALSE) #write for use combination with model subset data
+write.csv(pt_covs, paste0(outfolder,"/med_pcp_hillshade.csv"), row.names=FALSE) #write for use combination with model subset data
