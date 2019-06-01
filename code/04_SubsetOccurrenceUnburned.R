4a63c8abbbbf76a654f4c89b920d488d4408cf2a
mattwilliamson13 <mwilliamson@ucdavis.edu>
2019-06-01 16:50:30 -0700

    fix pathnames

--- code/04_SubsetOccurrenceUnburned.R
+++ code/04_SubsetOccurrenceUnburned.R
@@ -1,11 +1,11 @@
 ##This code subsets the original merged dataset for use in a bernoulli model to explain BRTE occurrence on unburned sites only (to identify other important factors in the absence of fire). Presence in this analysis refers to the first year in which sampling detected BRTE. Absence refers to the final year of sampling for points at which BRTE was never detected. Models do not use perennial grass frequency as the number of missing data points dramatically reduces sample size.
 library(rethinking) #for coerce_index function
 library(tidyverse)
-infolder <- "/Users/matthewwilliamson/Google Drive/GB_Final/processed_data/" #data is already processed so both inputs and outputs in processed folder
-outfolder <- "/Users/matthewwilliamson/Google Drive/GB_Final/processed_data/"
+infolder <- here::here("processed_data/") #data is already processed so both inputs and outputs in processed folder
+outfolder <- here::here("processed_data/")
 
 # load original data frame
-db.or <-read.csv(file =paste0(infolder,"alldatamerged.csv"))
+db.or <-read.csv(file =paste0(infolder,"/alldatamerged.csv"))
 db.or <- db.or[order(db.or$yr),] #order by year
 
 #Subset dataframe for occurrence analysis
@@ -47,12 +47,12 @@ db.mod.ub <- db.mod.ub[,c(3:4,1,2,17,8,10,15:16,18)]
 db.mod.2 <- db.mod.ub[complete.cases(db.mod.ub),] #none correlated at greater than 0.7 all retained
 
 db.occ.merge <- db.mod.2
-pt_covs <- read.csv(file=paste0(infolder,"med_pcp_hillshade.csv"))
+pt_covs <- read.csv(file=paste0(infolder,"/med_pcp_hillshade.csv"))
 
 db_join <- db.occ.merge %>% left_join(pt_covs, by=c("range", "area","pt"))
 db_join <- db_join[,-c(8:10)]
 
-write.csv(db_join, paste0(outfolder,"occurence_model_ub_hillshade_noscale.csv"), row.names=FALSE) #updated for new models
+write.csv(db_join, paste0(outfolder,"/occurence_model_ub_hillshade_noscale.csv"), row.names=FALSE) #updated for new models
 
 db_join$elev <- scale(db_join$elev)
 db_join$medwtpcp <- scale(db_join$medwtpcp)
@@ -66,4 +66,4 @@ db.mod.2$area_id<- coerce_index(db.mod.2$area)
 db.mod.2$point_id<- coerce_index(db.mod.2$pt)
 
 
-write.csv(db.mod.2, paste0(outfolder, "occurence_model_unburned_hillshade.csv" ), row.names=FALSE) #updated for new models
+write.csv(db.mod.2, paste0(outfolder, "/occurence_model_unburned_hillshade.csv" ), row.names=FALSE) #updated for new models
