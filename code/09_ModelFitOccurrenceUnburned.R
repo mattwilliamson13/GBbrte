28f67fcdc36860886cc069f33de68edaaa920dd0
mattwilliamson13 <mwilliamson@ucdavis.edu>
2019-06-01 17:29:28 -0700

    fix pathnames

--- code/09_ModelFitOccurrenceUnburned.R
+++ code/09_ModelFitOccurrenceUnburned.R
@@ -1,11 +1,12 @@
 library(rstan)
 library(shinystan)
 rstan_options(auto_write = TRUE)
+Sys.setenv(LOCAL_CPPFLAGS = '-march=native')
 options(mc.cores = parallel::detectCores())
-infolder <- "/Users/matthewwilliamson/Google Drive/GB_Final/processed_data/" #data is already processed so both inputs and outputs in processed folder
-outfolder <- "/Users/matthewwilliamson/Google Drive/GB_Final/model_fits/"
-stanfolder <- "/Users/matthewwilliamson/Google Drive/GB_Final/stan_models/"
-db.stan <- read.csv(file=paste0(infolder,"occurence_model_unburned_hillshade.csv"), stringsAsFactors = FALSE)
+infolder <- here::here("processed_data/") #data is already processed so both inputs and outputs in processed folder
+outfolder <- here::here("model_fits/")
+stanfolder <- here::here("stan_models/")
+db.stan <- read.csv(file=paste0(infolder,"/occurence_model_unburned_hillshade.csv"), stringsAsFactors = FALSE)
 
 #set up indices for nested effects
 can.rg <- db.stan[,12:13]
@@ -22,6 +23,6 @@ occ_data_ub <- list(N=nrow(db.stan),R=length(unique(db.stan$range_id)),C=length(
             y=db.stan$occ, W = as.matrix(db.stan[,c(6,8:9,11)]), X = db.stan[,7], J = ncol(db.stan[,c(6,8:9,11)]), K = 1) #get rid of spring precip again
 
 
-occ_fit_ub <- stan(paste0(stanfolder,"occurrence_asp_ub.stan"), data=occ_data_ub, seed=082980, control=list(adapt_delta = 0.95))
+occ_fit_ub <- stan(paste0(stanfolder,"/occurrence_asp_ub.stan"), data=occ_data_ub, seed=082980, control=list(adapt_delta = 0.95))
 
-save(occ_fit_ub, file=paste0(outfolder,"occ_fit_ub_hillshade.RData"))
+save(occ_fit_ub, file=paste0(outfolder,"/occ_fit_ub_hillshade.RData"))
